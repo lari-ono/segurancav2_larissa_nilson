89343aaa9f2694a026d5cefdcc73e76138078ffe
Larissa Hikari Ono <lari.hk.ono@gmail.com>
2023-11-17 01:25:52 +0000

    inclusão de arquivos

---
+++ app/src/main/java/application/service/AppUserDetailsService.java
@@ -0,0 +1,29 @@
+package application.service;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user==null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+        UserDetails = org.springframework.security.core.userdetails.User.builder()
+        .username(user.getUsername());
+        .password(user.getPassword());
+        .roles("USER");
+        .build();
+
+    return userDetails;
+    
+    }
+
+}
